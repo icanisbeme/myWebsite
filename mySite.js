103d4bf7d19187baba022d03554f27b0ce2d890f
unknown <icanisbeme@gmail.com>
2020-09-02 00:45:07 -0400

    got rid of some excess comments

--- mySite.js
+++ mySite.js
@@ -7,14 +7,14 @@ window.smoothScroll = function(target) {
 
   document.getElementById("factsMasterDiv").style.display = "block";
 
-  do { //find scroll container
+  do { 
       scrollContainer = scrollContainer.parentNode;
       if (!scrollContainer) return;
       scrollContainer.scrollTop += 1;
   } while (scrollContainer.scrollTop == 0);
   
   var targetY = 0;
-  do { //find the top of target relatively to the container
+  do {
       if (target == scrollContainer) break;
       targetY += target.offsetTop;
   } while (target = target.offsetParent);
